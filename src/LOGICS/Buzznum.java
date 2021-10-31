d49ff824fedd952d28914bfda817080b6bee7d87
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-10-31 19:13:59 +0530

    CREATED BY ANKITA

--- src/LOGICS/Buzznum.java
+++ src/LOGICS/Buzznum.java
@@ -1,4 +1,15 @@
 package LOGICS;
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
+
 import java.util.Scanner;
 public class Buzznum {
     public boolean buzznumber(int a){
