7480a07fd471796ed67835b4fa372804b95208ff
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-10-31 19:21:22 +0530

    CREATED BY ANKITA

--- src/LOGICS/Palindrome_no.java
+++ src/LOGICS/Palindrome_no.java
@@ -1,4 +1,14 @@
 package LOGICS;
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 
 public class Palindrome_no
 {
