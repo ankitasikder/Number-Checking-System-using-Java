3d4a54b288c38266622ce94d475992119919c29a
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-10-31 19:15:00 +0530

    CREATED BY ANKITA

--- src/LOGICS/Disariumnum.java
+++ src/LOGICS/Disariumnum.java
@@ -6,6 +6,16 @@ public class Disariumnum {
         int copy = num, d = 0, sum = 0;
         String s = Integer.toString(num);
         int len = s.length();
+/*ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com
+*/
 
         while(copy>0)
         {
